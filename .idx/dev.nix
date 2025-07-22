ac1150242ffaed4ad7ea192b4d93a29b32bbecd2
haswinginvesting <haswinginvesting@gmail.com>
2025-07-22 03:11:27 +0000

    one more change to nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,19 +1,8 @@
-# To learn more about this file, see https://developer.idx.xyz/docs/configuration/
 { pkgs, ... }: {
-  # Which nix channel to use.
-  channel = "stable-23.11"; # Or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-23.11";
   packages = [
-    pkgs.nodejs_20 # Pinned to the latest Node.js 20 version
+    pkgs.nodejs_20
   ];
-  # Sets environment variables in the workspace
-  env = {};
-  # Fast-starts services on port openings
-  services.ports = [];
-  # Manages VNC-based previews (like Android Emulators)
-  services.vnc = {};
-
-  # Defines scripts to be run on workspace startup
   idx.previews = {
     previews = [{
       command = [ "npm" "run" "dev" "--" "--host" "0.0.0.0" ];
